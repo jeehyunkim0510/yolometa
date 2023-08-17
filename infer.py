ddc8d4470bc3b5ae17c00c7a4b45e36d50874750
jeehyun <jeehyunkim0510@naver.com>
2023-08-17 11:11:10 +0900

    revise

--- infer.py
+++ infer.py
@@ -4,14 +4,14 @@ from PIL import Image
 from ultralytics import YOLO
 
 # Load a pretrained YOLOv8n model
-model = YOLO('yolov8n.pt')
+model = YOLO(r'C:\Users\user\PycharmProjects\pythonProject4\runs\detect\train\weights\best.pt')
 
 # Run inference on 'bus.jpg'
-results = model('bus.jpg')  # results list
+results = model(r'C:\Users\user\PycharmProjects\pythonProject4\Fish-44\test\images\FishDataset22_png.rf.db11b2a5d8d8a1e31fe1414981840ead.jpg')  # results list
 
 # Show the results
 for r in results:
     im_array = r.plot()  # plot a BGR numpy array of predictions
     im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
     im.show()  # show image
-    im.save('results.jpg')  # save image
+    im.save('InferImage/results.jpg')  # save image
